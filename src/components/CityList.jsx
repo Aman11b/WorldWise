23c5a7dad3de05e9a0b09234369454c979d24f30
Aman11b <amansingh1100b@gmail.com>
2026-04-23 18:31:24 +0530

    consuming citiesContext

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -2,8 +2,10 @@ import styles from "../components/CityList.module.css";
 import Spinner from "./Spinner";
 import CityItem from "./CityItem";
 import Message from "./Message";
+import { useCities } from "../Context/CitiesContent";
 
-function CityList({ cities, isLoading }) {
+function CityList() {
+  const { cities, isLoading } = useCities();
   if (isLoading) return <Spinner />;
 
   if (!cities.length)
